4d2eb09e77e0a6aca0259a5fab3a2d8286640737
Elaidzha1940 <eli080293@gmail.com>
2024-04-03 23:41:10 +0300

    Update Defaults

--- Defaults/ViewController.swift
+++ Defaults/ViewController.swift
@@ -21,29 +21,38 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         saveButton.addTarget(self, action: #selector(didTapSaveButton), for: .touchUpInside)
         deleteButton.addTarget(self, action: #selector(didTapDeleteButton), for: .touchUpInside)
-        segmentedControl .addTarget(self, action: #selector(didTapSegmentedControl), for: .valueChanged)
+        segmentedControl.addTarget(self, action: #selector(didTapSegmentedControl), for: .valueChanged)
         
         saveCurrentLaunch()
-        statusLabel.text = "Run App: \(UserDefaults.standard.integer(forKey: "NumberLaunches"))"
-        textField.text =  UserDefaults.standard.string(forKey: "InputText")
+        statusLabel.text = "Run App: \(UserDefaults.standard.integer(forKey: numberOfLaunchesKey))"
+        textField.text =  UserDefaults.standard.string(forKey: inputTextKey)
+        segmentedControl.selectedSegmentIndex = UserDefaults.standard.integer(forKey: segmentIndex)
     }
     
     @objc func didTapSaveButton()   {
         guard let text = textField.text else { return }
-        UserDefaults.standard.set(text, forKey: "InputText")
+        UserDefaults.standard.set(text, forKey: inputTextKey)
     }
     
     @objc func didTapDeleteButton()   {
-        print("delete")
+        let keys = [numberOfLaunchesKey, inputTextKey, segmentIndex]
+        keys.forEach {
+            UserDefaults.standard.set(nil, forKey: $0)
+        }
     }
     
     @objc func didTapSegmentedControl(sender: UISegmentedControl)   {
-        print(sender.selectedSegmentIndex)
+        UserDefaults.standard.set(sender.selectedSegmentIndex, forKey: segmentIndex)
     }
     
     func saveCurrentLaunch() {
-        let index = UserDefaults.standard.integer(forKey: "NumberLaunches")
-        UserDefaults.standard.set(index + 1, forKey: "NumberLaunches")
+        let index = UserDefaults.standard.integer(forKey: numberOfLaunchesKey)
+        UserDefaults.standard.set(index + 1, forKey: numberOfLaunchesKey)
     }
+    
+    // MARK: - Private constants
+    private let numberOfLaunchesKey = "NumberOfLaunches"
+    private let inputTextKey = "InputText"
+    private let segmentIndex = "SegmentControlIndex"
 }
 
