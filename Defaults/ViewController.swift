e6268d593adad932a4f837440f7c737d3200519b
Elaidzha1940 <eli080293@gmail.com>
2024-04-03 20:56:55 +0300

    Update Main

--- Defaults/ViewController.swift
+++ Defaults/ViewController.swift
@@ -1,19 +1,25 @@
+//  /*
 //
-//  ViewController.swift
-//  Defaults
+//  Project: Defaults
+//  File: ViewController.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 02.04.2024
 //
-//  Created by Elaidzha Shchukin on 02.04.2024.
-//
+//  */
 
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet private var statusLabel: UILabel!
+    @IBOutlet private var textField: UITextField!
+    @IBOutlet private var saveButton: UIButton!
+    @IBOutlet private var segmentedControl: UISegmentedControl!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //
     }
-
-
 }
 
