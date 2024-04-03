75e8c92645e8998c101e076157bcdfc97ae55110
Elaidzha1940 <eli080293@gmail.com>
2024-04-03 23:23:34 +0300

    Update ViewController

--- Defaults/ViewController.swift
+++ Defaults/ViewController.swift
@@ -21,20 +21,29 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         saveButton.addTarget(self, action: #selector(didTapSaveButton), for: .touchUpInside)
         deleteButton.addTarget(self, action: #selector(didTapDeleteButton), for: .touchUpInside)
-        segmentedControl .addTarget(self, action: #selector(didTapDeleteButton), for: .valueChanged)
+        segmentedControl .addTarget(self, action: #selector(didTapSegmentedControl), for: .valueChanged)
+        
+        saveCurrentLaunch()
+        statusLabel.text = "Run App: \(UserDefaults.standard.integer(forKey: "NumberLaunches"))"
+        textField.text =  UserDefaults.standard.string(forKey: "InputText")
     }
     
     @objc func didTapSaveButton()   {
-        
+        guard let text = textField.text else { return }
+        UserDefaults.standard.set(text, forKey: "InputText")
     }
     
     @objc func didTapDeleteButton()   {
-        
+        print("delete")
     }
     
-    @objc func didTapSegmentedControl()   {
-        
+    @objc func didTapSegmentedControl(sender: UISegmentedControl)   {
+        print(sender.selectedSegmentIndex)
     }
     
+    func saveCurrentLaunch() {
+        let index = UserDefaults.standard.integer(forKey: "NumberLaunches")
+        UserDefaults.standard.set(index + 1, forKey: "NumberLaunches")
+    }
 }
 
