228863b5bdeaed5063db8143f4277054931151f2
Elaidzha1940 <eli080293@gmail.com>
2024-04-03 22:19:19 +0300

    Update ViewController

--- Defaults/ViewController.swift
+++ Defaults/ViewController.swift
@@ -15,11 +15,26 @@ class ViewController: UIViewController {
     @IBOutlet private var textField: UITextField!
     @IBOutlet private var saveButton: UIButton!
     @IBOutlet private var segmentedControl: UISegmentedControl!
+    @IBOutlet private var deleteButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        //
+        saveButton.addTarget(self, action: #selector(didTapSaveButton), for: .touchUpInside)
+        deleteButton.addTarget(self, action: #selector(didTapDeleteButton), for: .touchUpInside)
+        segmentedControl .addTarget(self, action: #selector(didTapDeleteButton), for: .valueChanged)
     }
+    
+    @objc func didTapSaveButton()   {
+        
+    }
+    
+    @objc func didTapDeleteButton()   {
+        
+    }
+    
+    @objc func didTapSegmentedControl()   {
+        
+    }
+    
 }
 
