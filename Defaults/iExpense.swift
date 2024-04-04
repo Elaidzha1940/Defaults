58f5822436a2f78879925acc0a26db9473c9c752
Elaidzha1940 <eli080293@gmail.com>
2024-04-05 00:04:23 +0300

    Added iExpense

---
+++ Defaults/iExpense.swift
@@ -0,0 +1,23 @@
+//  /*
+//
+//  Project: Defaults
+//  File: iExpense.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 05.04.2024
+//
+//  */
+ 
+import SwiftUI
+
+struct iExpense: View {
+    var body: some View {
+        
+        VStack {
+            
+        }
+    }
+}
+
+#Preview {
+    iExpense()
+}
